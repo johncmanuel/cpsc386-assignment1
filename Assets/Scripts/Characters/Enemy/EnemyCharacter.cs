5f3a9409a2eca287c193476054d1afa91a2ab2f8
spicycurrey <spiceycurrey@gmail.com>
2024-02-26 20:53:52 -0800

    Update EnemyCharacter.cs

--- Assets/Scripts/Characters/Enemy/EnemyCharacter.cs
+++ Assets/Scripts/Characters/Enemy/EnemyCharacter.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class EnemyCharacter : MonoBehaviour, ICharacter
 {
+    public float Health { get; set; }
 
+    public void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Move(Vector2 direction)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TakeDamage(float amount)
+    {
+        throw new System.NotImplementedException();
+    }
 }
