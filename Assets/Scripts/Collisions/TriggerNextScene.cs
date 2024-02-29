24b4baa5d06010656de9deba88873b150204f416
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-02-29 10:10:29 -0800

    worked on dungeon2 (working somewhat)

--- Assets/Scripts/Collisions/TriggerNextScene.cs
+++ Assets/Scripts/Collisions/TriggerNextScene.cs
@@ -8,12 +8,27 @@ public class TriggerNextScene : MonoBehaviour
     [SerializeField]
     private GameManager gameManager;
 
+    [SerializeField]
+    private string sceneName;
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("Collision detected with: " + col.gameObject.tag);
         if (col.gameObject.tag == "Player")
         {
-            gameManager.SwitchToScene("Dungeon2");
+            if (gameManager == null)
+            {
+                Debug.LogError("Game Manager not set in TriggerNextScene");
+                return;
+            }
+
+            if (sceneName == null)
+            {
+                Debug.LogError("Scene Name not set in TriggerNextScene");
+                return;
+            }
+
+            gameManager.SwitchToScene(sceneName);
         }
     }
 
