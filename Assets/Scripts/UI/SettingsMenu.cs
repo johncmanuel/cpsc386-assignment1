6031e44631bbb03dcb78d8489686dc2c292eeffd
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-04-19 10:13:24 -0700

    fix volume bug

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -7,7 +7,12 @@ using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour
 {
     [SerializeField] private AudioMixer mainAudioMixer;
+
     [SerializeField] private Slider masterVolumeSlider;
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Slider SFXVolumeSlider;
+
+    // Value in decibels
     private const float DefaultVolume = -15f;
 
     // Start off as active, then deactivate after loading volume values from Prefs
@@ -21,30 +26,70 @@ public class SettingsMenu : MonoBehaviour
 
         InitializeVolume();
 
-        gameObject.SetActive(false);
+        // gameObject.SetActive(false);
+        var settingsScrollView = GameObject.Find("Settings Scroll View");
+        settingsScrollView.SetActive(false);
     }
+
+    // private void OnDisable()
+    // {
+    //     PlayerPrefs.Save();
+    // }
 
     private void InitializeVolume()
     {
-        float masterVolumePrefs = PlayerPrefs.GetFloat(AudioNames.MasterVolume);
-        if (masterVolumePrefs == -1f)
-        {
-            Debug.Log("Master volume not found in prefs, setting to default");
-            mainAudioMixer.SetFloat(AudioNames.MasterVolume, DefaultVolume);
-        }
-        else
-        {
-            Debug.Log("Master volume found in prefs, setting to " + masterVolumePrefs);
-            mainAudioMixer.SetFloat(AudioNames.MasterVolume, masterVolumePrefs);
-        }
+        // float masterVolumePrefs = PlayerPrefs.GetFloat(AudioNames.MasterVolume);
+        // if (masterVolumePrefs == -1f)
+        // {
+        //     Debug.Log("Master volume not found in prefs, setting to default");
+        //     mainAudioMixer.SetFloat(AudioNames.MasterVolume, DefaultVolume);
+        // }
+        // else
+        // {
+        //     Debug.Log("Master volume found in prefs, setting to " + masterVolumePrefs);
+        //     mainAudioMixer.SetFloat(AudioNames.MasterVolume, masterVolumePrefs);
+        // }
 
-        masterVolumeSlider.value = masterVolumePrefs == -1f ? DefaultVolume : masterVolumePrefs;
+        // masterVolumeSlider.value = masterVolumePrefs == -1f ? DefaultVolume : masterVolumePrefs;
+        SetVolumeFromPrefs(AudioNames.MasterVolume, masterVolumeSlider);
+    }
+
+    private void SetVolumeFromPrefs(string audioName, Slider slider)
+    {
+        float volumePrefs = PlayerPrefs.GetFloat(audioName, DefaultVolume);
+        mainAudioMixer.SetFloat(audioName, volumePrefs);
+
+        slider.value = volumePrefs == -1f ? DefaultVolume : volumePrefs;
     }
 
     public void SetMasterVolume(float volume)
     {
-        mainAudioMixer.SetFloat(AudioNames.MasterVolume, volume);
-        PlayerPrefs.SetFloat(AudioNames.MasterVolume, volume);
+        SaveVolumeSettings(AudioNames.MasterVolume, volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        SaveVolumeSettings(AudioNames.MusicVolume, volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        SaveVolumeSettings(AudioNames.SFXVolume, volume);
+    }
+
+    public void SaveVolumeSettings(string volumeName, float volume)
+    {
+        volume = LinearToDecibel(volume);
+        mainAudioMixer.SetFloat(volumeName, volume);
+        PlayerPrefs.SetFloat(volumeName, volume);
         PlayerPrefs.Save();
+        Debug.Log("Saved " + volumeName + " as " + volume);
+    }
+
+    // Source
+    // https://discussions.unity.com/t/how-to-convert-decibel-db-number-to-audio-source-volume-number-0to1/46543/4
+    private float LinearToDecibel(float linear, float minDecibelVal = -80.0f)
+    {
+        return linear != 0 ? 20.0f * Mathf.Log10(linear) : minDecibelVal;
     }
 }
