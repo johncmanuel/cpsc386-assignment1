a0d0d722272bcb046b2d8254f49b1b5f34bc2ce5
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-04-29 22:08:56 -0700

    player anim

---
+++ Assets/Scripts/Characters/Player/PlayerAnim.cs
@@ -0,0 +1,7 @@
+
+public static class PlayerAnim
+{
+    public const string AnimMoveX = "AnimMoveX";
+    public const string AnimMoveY = "AnimMoveY";
+    public const string AnimMoveMagnitude = "AnimMoveMagnitude";
+}
